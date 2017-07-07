4f694f2827c6f00b9ff7f761a9318184e269172d
Lou Dog <loudoguno@gmail.com>
2017-07-07 00:57:37 -0500

    Beefed up osx and brew.sh

--- lou-brew.sh
+++ lou-brew.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-# lou-brew
 
 # Ask for the administrator password upfront.
 sudo -v
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -14,34 +21,36 @@ brew upgrade --all
 brew install blueutil
 
 
-# Casks
+# Casks# Install Cask
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" java
+brew cask install --appdir="~/Applications" xquartz
+
 brew cask install --appdir="~/Applications" dropbox
-brew cask install --appdir="~/Applications" google-chrome
-brew cask install --appdir="~/Applications" alfred
 
 brew cask install --appdir="~/Applications" seil
 brew cask install --appdir="~/Applications" karabiner
 brew cask install --appdir="~/Applications" keyboard-maestro
 brew cask install --appdir="~/Applications" bettertouchtool
 brew cask install --appdir="~/Applications" slate
+brew cask install --appdir="~/Applications" alfred
+brew cask install --appdir="~/Applications" bartender
 
 brew cask install --appdir="~/Applications" google-chrome
 brew cask install --appdir="~/Applications" firefox
 brew cask install --appdir="~/Applications" evernote
 brew cask install --appdir="~/Applications" sublime
-
-
+brew cask install --appdir="~/Applications" pocket
 
 brew cask install --appdir="~/Applications" snagit
-
 brew cask install --appdir="~/Applications" vlc
 brew cask install --appdir="~/Applications" spotify
 brew cask install --appdir="~/Applications" keepassx
 
-
 brew cask install --appdir="~/Applications" appcleaner
-
-
 
 
 
--- lou-osx.sh
+++ lou-osx.sh
@@ -813,7 +813,8 @@ defaults write com.operasoftware.OperaDeveloper PMPrintingExpandedStateForPrint2
 ###############################################################################
 
 # Install Sublime Text settings
-cp -r init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+cp -r sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+
 
 ###############################################################################
 # Transmission.app                                                            #
